a888815e697ad0ea61069b968911f827fc75213c
haris-afr <haris.afridi1312@gmail.com>
2025-06-26 22:05:43 +0500

    make comp choose code

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 console.log("Hello World!")
+
+function getComputerChoice(){
+    const choice = Math.round((Math.random() * 3 + 0.5));
+    console.log(choice)
+    switch (choice){
+        case 1:
+            return "rock";
+            break;
+        case 2:
+            return "paper";
+            break;
+        case 3:
+            return "scissors";
+            break;
+        default:
+            return "error!";
+    }
+}
+
+console.log(getComputerChoice());
+
+
+// ones = 0
+// twos = 0
+// threes = 0
+// for (i = 0; i < 10000; i++){
+//     const choice = Math.round((Math.random() * 3 + 0.5));
+//     switch (choice){
+//         case 1:
+//             ones++;
+//             break;
+//         case 2:
+//             twos++;
+//             break;
+//         case 3:
+//             threes++;
+//             break;
+//         default:
+//             console.log("error!");
+//     }
+// }
+// console.log(ones);
+// console.log(twos);
+// console.log(threes);
